380c0a94419ffc434057bb5d5a89aa4e945f0ea1
Nethees <netheeswaran.arunachalam@ucdconnect.ie>
2025-06-04 21:42:01 +0100

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
+
 
 from src.exception import CustomException
 
@@ -15,5 +17,38 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate the performance of multiple regression models and return a report.
+    
+    Parameters:
+    - X_train: Training feature set
+    - y_train: Training target variable
+    - X_test: Testing feature set
+    - y_test: Testing target variable
+    - models: Dictionary of model names and their corresponding model instances
+    
+    Returns:
+    - model_report: Dictionary with model names as keys and their R2 scores as values
+    """
+    try:
+        
+        model_report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train) # Training the model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            model_report[list(models.keys())[i]] = test_model_score
+    
+        return model_report
     except Exception as e:
         raise CustomException(e, sys)
